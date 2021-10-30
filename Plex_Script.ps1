b585909fb3f869787805bc244b5fa43081228084
Twikki <46316044+Twikki@users.noreply.github.com>
2021-10-30 11:29:42 +0200

    Create Plex_Script.ps1

---
+++ Plex_Script.ps1
@@ -0,0 +1,18 @@
+<#
+    .NOTES
+    ===========================================================================
+     Modified on:   30-10-2021
+     Created on:   	30-10-2021
+	 Created by:   	Daniel Jean Schmidt
+	 Organization: 	Dajs
+     Filename:     	Plex_Script.ps1
+	===========================================================================
+    ===========================================================================
+     Requirements: 
+     - Can be run on Windows 10, Server 2019, Server 2022
+    ===========================================================================
+    .DESCRIPTION
+    This script gives different possibilities to manipulate Plex running on Windows
+#>
+
+
